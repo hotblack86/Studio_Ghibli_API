e96f45cc767dae2636d1d92a664e3582698dd6de
James Hotblack <james.c.hotblack@gmail.com>
2019-09-05 18:16:53 +0100

    App now randomly uses either XHRor Fetch to handle the API.

--- script.js
+++ script.js
@@ -9,39 +9,43 @@ container.setAttribute('class', 'container')
 app.appendChild(logo)
 app.appendChild(container)
 
+const rand = Math.random() < 0.5;
 
 // API Handling with XHR
-// var request = new XMLHttpRequest()
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-// request.onload = function() {
-//   // Begin accessing JSON data here
-//   var data = JSON.parse(this.response)
-//   if (request.status >= 200 && request.status < 400) {
-//     data.forEach(movie => {
-//       const card = document.createElement('div')
-//       card.setAttribute('class', 'card')
+if (rand) {
+  var request = new XMLHttpRequest()
+  request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+  request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+    if (request.status >= 200 && request.status < 400) {
+      console.log("Using XHR!")
+      data.forEach(movie => {
+        const card = document.createElement('div')
+        card.setAttribute('class', 'card')
 
-//       const h1 = document.createElement('h1')
-//       h1.textContent = movie.title
+        const h1 = document.createElement('h1')
+        h1.textContent = movie.title
 
-//       const p = document.createElement('p')
-//       movie.description = movie.description.substring(0, 300)
-//       p.textContent = `${movie.description}...`
+        const p = document.createElement('p')
+        movie.description = movie.description.substring(0, 300)
+        p.textContent = `${movie.description}...`
 
-//       container.appendChild(card)
-//       card.appendChild(h1)
-//       card.appendChild(p)
-//     })
-//   } else {
-//     console.log('error')
-//   }
-// }
-// request.send()
-
+        container.appendChild(card)
+        card.appendChild(h1)
+        card.appendChild(p)
+      })
+    } else {
+      console.log('error')
+    }
+  }
+  request.send()
+} else {
 // API Handling with Fetch
-fetch('https://ghibliapi.herokuapp.com/film')
+fetch('https://ghibliapi.herokuapp.com/films')
 	.then(function (response) {
-		return response.json();
+    console.log("Using Fetch!");
+    return response.json();
 	})
 	.then(function (data) {
 		data.forEach(movie => {
@@ -63,3 +67,4 @@ fetch('https://ghibliapi.herokuapp.com/film')
 	.catch(function (error) {
 		console.log('error', error);
 	});
+}
