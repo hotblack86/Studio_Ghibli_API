8b10d655b62e788c13fc5ddcc3db484965860f3c
James Hotblack <james.c.hotblack@gmail.com>
2019-09-05 17:49:09 +0100

    Attempting to convert app to usefetch instead of xhr - success, data array is being console logged.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// DOM
 const app = document.getElementById('root')
-
 const logo = document.createElement('img')
 logo.src = 'logo.png'
 
@@ -9,32 +9,45 @@ container.setAttribute('class', 'container')
 app.appendChild(logo)
 app.appendChild(container)
 
-var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
 
-      const h1 = document.createElement('h1')
-      h1.textContent = movie.title
+// API Handling with XHR
+// var request = new XMLHttpRequest()
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+// request.onload = function() {
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response)
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach(movie => {
+//       const card = document.createElement('div')
+//       card.setAttribute('class', 'card')
 
-      const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
+//       const h1 = document.createElement('h1')
+//       h1.textContent = movie.title
 
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-    })
-  } else {
-    const errorMessage = document.createElement('err')
-    errorMessage.textContent = `It's not working!`
-    app.appendChild(errorMessage)
-  }
-}
+//       const p = document.createElement('p')
+//       movie.description = movie.description.substring(0, 300)
+//       p.textContent = `${movie.description}...`
 
-request.send()
+//       container.appendChild(card)
+//       card.appendChild(h1)
+//       card.appendChild(p)
+//     })
+//   } else {
+//     const errorMessage = document.createElement('err')
+//     errorMessage.textContent = `It's not working!`
+//     app.appendChild(errorMessage)
+//   }
+// }
+// request.send()
+
+// API Handling with Fetch
+fetch('https://ghibliapi.herokuapp.com/films')
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log('success', data);
+	})
+	.catch(function (error) {
+		console.log('error', error);
+	});
