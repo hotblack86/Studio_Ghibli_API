914f908c76c19ea0b7054e17cbe84815e1ae0ade
James Hotblack <james.c.hotblack@gmail.com>
2019-09-05 17:58:46 +0100

    Full functionality using fetch method.

--- script.js
+++ script.js
@@ -33,20 +33,32 @@ app.appendChild(container)
 //       card.appendChild(p)
 //     })
 //   } else {
-//     const errorMessage = document.createElement('err')
-//     errorMessage.textContent = `It's not working!`
-//     app.appendChild(errorMessage)
+//     console.log('error')
 //   }
 // }
 // request.send()
 
 // API Handling with Fetch
-fetch('https://ghibliapi.herokuapp.com/films')
+fetch('https://ghibliapi.herokuapp.com/film')
 	.then(function (response) {
 		return response.json();
 	})
 	.then(function (data) {
-		console.log('success', data);
+		data.forEach(movie => {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+      
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+      
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0, 300)
+      p.textContent = `${movie.description}...`
+      
+      container.appendChild(card)
+      card.appendChild(h1)
+      card.appendChild(p)
+          })
 	})
 	.catch(function (error) {
 		console.log('error', error);
